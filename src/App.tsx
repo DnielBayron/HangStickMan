ac0ff0a62f2ba916fa76e2f0a86db27491b1985e
deniel bayron <dpbayron@gmail.com>
2023-11-03 23:14:45 +0800

    enter button incomplete

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ const App: React.FC = () => {
     return words[Math.floor(Math.random() * words.length)]
     
   })
+ 
   console.log(wordToGuess);
   
 
@@ -80,10 +81,10 @@ const App: React.FC = () => {
       <div className="max-w-[1280px] flex flex-col gap-8 items-center m-auto">
                 <div className="mt-5 p-4 text-center">
                 {isWinner && <div className="text-red-500 xl:font-bold text-4xl xl:5xl font-Play "> 🎉 CONGRATS! ✔ 🎉 <div className="mt-4 text-2xl"><button 
-                onClick={() => setGuessedLetters([])}
+                
                 className="text-green-500 p-2  bg-gray-500 rounded-lg ">Enter</button> <span className=" text-black">to Refresh</span></div></div> }
                 {isLoser &&  <div className="text-red-500 font-bold  text-4xl xl:text-5xl font-Play "> TRY AGAIN! 😭 <div className="mt-4 text-2xl"><button 
-                onClick={() => setGuessedLetters([])}
+               
                 className="text-green-500 xl:p-2 p-1   bg-gray-500 rounded-lg ">Enter</button> <span className=" text-black">to Refresh</span></div></div> }
                 </div>
 
